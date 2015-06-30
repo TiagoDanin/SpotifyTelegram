abbb94bf11745263048c5e628ac4254f0809e248
Tiago Danin <tiagojdanin@hotmail.com>
2015-06-30 11:34:31 -0300

    Update spotifytrack.lua
    Thanks @ariefbayu Arief Bayu Purwanto

--- spotifytrack.lua
+++ spotifytrack.lua
@@ -2,10 +2,10 @@ do
 
 local BASE_URL = "https://api.spotify.com/v1/search"
 
-local function get_spotify(Track)
-  print("Spotify, ", Track)
+local function get_spotify(STrack)
+  print("Spotify, ", STrack)
   local url = BASE_URL
-  url = url..'?q='..Track..'&type=track&limit=10'
+  url = url..'?q='..STrack..'&type=track&limit=10'
 
   local dec, t, h = http.request(url)
   if t ~= 200 then return nil end
@@ -18,8 +18,8 @@ end
 local function run(msg, matches)
   local Track = ''
 
-  if matches[1] ~= '!spotift' then 
-    STrack = matches[1]
+  if matches[1] ~= '!spotify' then 
+    Track = matches[1]
   end
   local text = get_spotify(STrack)..Surl
   if not text then
