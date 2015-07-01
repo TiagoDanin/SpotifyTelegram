d6958749af83251625d998d8adce282ae3f9b7da
Tiago Danin <tiagojdanin@hotmail.com>
2015-07-01 11:07:58 -0300

    Update spotifytrack.lua

--- spotifytrack.lua
+++ spotifytrack.lua
@@ -5,28 +5,28 @@ local BASE_URL = "https://api.spotify.com/v1/search"
 local function get_spotify(STrack)
   print("Spotify, ", STrack)
   local url = BASE_URL
-  url = url..'?q='..STrack..'&type=track&limit=1'
+  url = url..'?q='..URL.escape(STrack)
+  url = url..'&type=track&limit=1'
 
   local decj, tim, h = http.request(url)
   if tim ~= 200 then return nil end
 
   local spotify = json:decode(decj)
   local Track = spotify.tracks.items.name
-  local Surl = spotify.tracks.items.external_urls
-  local resu = 'Track is '..Track..' URL: '..Surl
+  local Surl = spotify.tracks.items.external_urls.spotify
+  local resu = 'Track is'..'Track..' URL: '..Surl
   
   return resu
 end
 
 local function run(msg, matches)
   local Track = ''
-
   if matches[1] ~= '!spotify' then 
     Track = matches[1]
   end
   local text = get_spotify(Track)
   if not text then
-    text = 'Erro'
+    text = 'Erro '..Track
   end
   return text
 end
@@ -36,7 +36,7 @@ return {
   usage = "!spotify (Track)",
   patterns = {
     "^!spotify$",
-    "^%.[s|S]potify (.*)$"
+    "^!spotify (.*)$"
   }, 
   run = run 
 }
