2a76b07a1a05c70ac03718d525fa329d24316a25
Tiago Danin <tiagojdanin@hotmail.com>
2015-07-03 14:20:50 -0300

    Updata
    Updata Build

--- spotifytrack.lua
+++ spotifytrack.lua
@@ -1,44 +1,46 @@
-do
+--Base
+--Get URL
+local function get_spotify(search)
+  local BASE_URL = "https://api.spotify.com/v1/search"
+  local URLP = "?q=".. (URL.escape(search) or "").."&type=track&limit=3" --limit 3
 
-local BASE_URL = "https://api.spotify.com/v1/search"
-
-local function get_spotify(STrack)
-  print("Spotify, ", STrack)
-  local url = BASE_URL
-  url = url..'?q='..URL.escape(STrack)
-  url = url..'&type=track&limit=1'
-
-  local decj, tim, h = http.request(url)
-  if tim ~= 200 then return nil end
-
+  --decode json
+  local decj, tim = https.request(BASE_URL..URLP)
+  if tim ~=200 then return nil  end
+  --Table
   local spotify = json:decode(decj)
-  local Track = spotify.tracks.items.name
-  local Surl = spotify.tracks.items.external_urls.spotify
-  local resu = 'Track is '..Track..' URL: '..Surl
-  
-  return resu
+  local tables = {}
+  for pri,result in ipairs(spotify.tracks.items) do
+    table.insert(tables, {
+        result.name,
+        result.external_urls.spotify
+      })
+  end
+  return tables
+end
+--Print table
+local function prints(tables)
+  local gets=""
+  for pri,cont in ipairs(tables) do
+    gets=gets.."▶️ "..cont[1].." - "..cont[2].."\n"
+  end
+  return gets
 end
 
 local function run(msg, matches)
-  local Track = ''
-  if matches[1] ~= '!spotify' then 
-    Track = matches[1]
+  local tables = get_spotify(matches[1])
+  if not tables then
+    tables = '🔴 Erro'
   end
-  local text = get_spotify(Track)
-  if not text then
-    text = 'Erro '..Track
-  end
-  return text
+  return prints(tables)
 end
 
 return {
-  description = "Track spotify", 
-  usage = "!spotify (Track)",
+  description = "✅ Track spotify byTiagoDanin",
+  usage = "!spotify + Name Track",
   patterns = {
     "^!spotify$",
     "^!spotify (.*)$"
-  }, 
-  run = run 
+  },
+  run = run
 }
-
-end
