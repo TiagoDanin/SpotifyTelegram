3db061aefb137bc7dd8d467155fb673bcf60e07f
Tiago Danin <tiagojdanin@hotmail.com>
2015-11-26 17:29:12 +0000

    Fixed bug and optimizer

--- BotBasedYagop/Spotify.lua
+++ BotBasedYagop/Spotify.lua
@@ -1,13 +1,18 @@
 -- Spotify Plugin for bot based on Yagop
--- ByTiagoDanin - Telegram.me/tiagodanin
-local function get_spotify(search)
-  local BASE_URL = "https://api.spotify.com/v1/search"
-  local URLP = "?q=".. (URL.escape(search) or "").."&type=track&limit=5" --limit 5
+-- BY: Telegram:@TiagoDanin -:- Twitter:@_TiagoEDGE
 
-  --Decode json
+function run(msg, matches)
+  local input = matches[1]
+  if input == "/spotify" or "/Spotify" or "!spotify" or "!Spotify" then -- FIX MATCHES
+    return "The name is missing"
+  end
+  --URL API
+  local BASE_URL = "https://api.spotify.com/v1/search"
+  local URLP = "?q=".. (URL.escape(input) or "").."&type=track&limit=5" -- Limit 5
+  -- Decode json
   local decj, tim = https.request(BASE_URL..URLP)
   if tim ~=200 then return nil  end
-  --Table
+  -- Table
   local spotify = json:decode(decj)
   local tables = {}
   for pri,result in ipairs(spotify.tracks.items) do
@@ -17,39 +22,28 @@ local function get_spotify(search)
         result.external_urls.spotify
       })
   end
-  return tables
-end
---Print table
-local function prints(tables)
-  -- Beta show number music:
-  --local total=""
-  --for pri,cont in ipairs(tables) do
-  --  total=cont[1]
-  --end
-  local gets=""
+  -- Prit Tables
+  local gets = ""
   for pri,cont in ipairs(tables) do
     gets=gets.."▶️ "..cont[2].." - "..cont[3].."\n"
   end
-  local thend = gets --.."Total "..total.."Music" -- OFF
-  return thend
+  -- ERRO 404
+  local text_end = gets -- Text END
+  if gets == "" then
+    text_end = "Not found music"
+  end
+  -- Send MSG
+  return text_end
 end
 
-local function run(msg, matches)
-  local tables = get_spotify(matches[1])
-  local text = prints(tables)
-  if text == "" then --Or if text == "Total  Music"
-    text = "Not found music" -- MSG Erro
-  end
-  return text
-end
 --Run
 return {
   description = "Track Spotify byTiagoDanin",
   usage = "!Spotify + Name Track",
   patterns = {
-    "^![Ss]potify$",
+    "^![Ss]potify$", -- Prefix !
     "^![Ss]potify (.*)$",
-    "^/[Ss]potify$",
+    "^/[Ss]potify$", -- Prefix /
     "^/[Ss]potify (.*)$",
   },
   run = run
